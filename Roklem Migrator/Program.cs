12d5eec18ab43c394494e83b989a33f37f280b1c
Gustaf Svensson <gussvens@kth.se>
2025-03-20 15:58:58 +0100

    Add service for parsing and reading file

--- Roklem Migrator/Program.cs
+++ Roklem Migrator/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Welcome to the Roklem Migrator");
+﻿using Roklem_Migrator.Services;
+using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath;
+
+        if (args.Length > 0)
+        {
+            filePath = args[0];
+        }
+        else
+        {
+            Console.WriteLine("No file path was given as argument, provide file path:");
+            filePath = Console.ReadLine();
+        }
+
+        var fileReader = new FileReaderService();
+
+        try
+        {
+            fileReader.ReadFile(filePath);
+        }
+        catch (FileNotFoundException)
+        {
+            Console.Error.WriteLine($"File not found at path: {filePath}");
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine($"An error occurred: {e.Message}");
+        }
+    }
+}
---
+++ Roklem Migrator/Services/FileReaderService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roklem_Migrator.Services
+{
+    class FileReaderService
+    {
+        public IEnumerable<string> ReadFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"File not found at path: {filePath}");
+            }
+
+            using (var reader = new StreamReader(filePath))
+            {
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+}
