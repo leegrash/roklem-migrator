ef4c1dae70c0a04ac4bf60872581d43d906862f3
Gustaf Svensson <gussvens@kth.se>
2025-03-25 16:11:06 +0100

    Setup DI

--- Roklem Migrator/Program.cs
+++ Roklem Migrator/Program.cs
@@ -1,8 +1,33 @@
 ﻿using Roklem_Migrator.Services;
+using Microsoft.Extensions.DependencyInjection;
+
 
 class Program
 {
     static void Main(string[] args)
+    {
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        var fileReader = serviceProvider.GetService<FileReaderService>();
+
+        if (fileReader != null)
+        {
+            RunApplication(args, fileReader);
+        }
+        else
+        {
+            Console.Error.WriteLine("FileReaderService could not be resolved.");
+        }
+    }
+
+    private static void ConfigureServices(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<FileReaderService>();
+    }
+
+    private static void RunApplication(string[] args, FileReaderService fileReader)
     {
         string filePath;
 
@@ -15,8 +40,6 @@ class Program
             Console.WriteLine("No file path was given as argument, provide file path:");
             filePath = Console.ReadLine() ?? string.Empty;
         }
-
-        var fileReader = new FileReaderService();
 
         try
         {
