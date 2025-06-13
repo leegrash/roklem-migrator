1fb8b67864cecc8de4883a4c59398329a66101ed
Gustaf Svensson <gussvens@kth.se>
2025-06-14 00:38:07 +0200

    Blank slate for a RoslynAnalyzerService

---
+++ Roklem Migrator/Services/RoslynAnalyzerService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Roklem_Migrator.Services.Interfaces;
+
+namespace Roklem_Migrator.Services
+{
+    internal class RoslynAnalyzerService : IRoslynAnalyzerService
+    {
+        public async Task<(bool, List<string>)> AnalyzeAsync(string slnPath)
+        {
+            var results = new List<string>();
+
+            
+
+            return (true, results);
+        }
+    }
+}
