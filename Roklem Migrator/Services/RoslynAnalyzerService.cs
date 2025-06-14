51893de125daba211f8655e34b41593e835c187a
Gustaf Svensson <gussvens@kth.se>
2025-06-14 12:04:32 +0200

    Loads solution without projects

--- Roklem Migrator/Services/RoslynAnalyzerService.cs
+++ Roklem Migrator/Services/RoslynAnalyzerService.cs
@@ -10,23 +10,24 @@ namespace Roklem_Migrator.Services
 {
     internal class RoslynAnalyzerService : IRoslynAnalyzerService
     {
-        public async Task<(bool, List<string>)> AnalyzeAsync(string projectPath)
+        public async Task<(bool, List<string>)> AnalyzeAsync(string slnFilePath)
         {
             var errors = new List<string>();
 
             using var workspace = MSBuildWorkspace.Create();
+            var solution = await workspace.OpenSolutionAsync(slnFilePath);
 
-            var project = await workspace.OpenProjectAsync(projectPath);
-
-
-            var compilation = await project.GetCompilationAsync();
-
-            var diagnostics = compilation.GetDiagnostics()
-                                         .Where(d => d.Severity == DiagnosticSeverity.Error || d.Severity == DiagnosticSeverity.Warning);
-
-            foreach (var diagnostic in diagnostics)
+            foreach (var project in solution.Projects.Where(p => p.Language == LanguageNames.VisualBasic))
             {
-                Console.WriteLine(diagnostic.ToString());
+                var compilation = await project.GetCompilationAsync();
+
+                var diagnostics = compilation.GetDiagnostics()
+                                             .Where(d => d.Severity == DiagnosticSeverity.Error || d.Severity == DiagnosticSeverity.Warning);
+
+                foreach (var diagnostic in diagnostics)
+                {
+                    errors.Add(diagnostic.ToString());
+                }
             }
 
             return (errors.Count == 0, errors);
